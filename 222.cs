49d04a29f5537e9cf5cbae9a24b1612bb1bf5dc8
Veronika123f <132203483+Veronika123f@users.noreply.github.com>
2023-04-30 14:58:13 +0300

    Add files via upload

---
+++ 222.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "The quick     brown   fox  jumps   over the  lazy  dog.";
+            int maxSpaces = 0;
+            int currentSpaces = 0;
+
+            foreach (char c in sentence)
+            {
+                if (c == ' ')
+                {
+                    currentSpaces++;
+                    if (currentSpaces > maxSpaces)
+                    {
+                        maxSpaces = currentSpaces;
+                    }
+                }
+                else
+                {
+                    currentSpaces = 0;
+                }
+            }
+
+            Console.WriteLine($"Найбільша кількість пробілів підряд: {maxSpaces}");
+            Console.ReadLine();
+
+        }
+    }
+}
